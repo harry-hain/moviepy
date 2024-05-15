b8ccdd799379573efa2510de29b3f852cbf5eb3b
Harry Hain <harryhain231@gmail.com>
2024-05-16 00:14:25 +0100

    Moved behavious closer to data containers whiloe refactoring ffmpeg_infos_parser.py

--- moviepy/video/io/ffmpeg_infos_parsing/file_metadata.py
+++ moviepy/video/io/ffmpeg_infos_parsing/file_metadata.py
@@ -1,3 +1,6 @@
+import re
+
+
 class FileMetadata:
     def __init__(self):
         self.result = {
@@ -12,3 +15,47 @@ class FileMetadata:
             "audio_found": False,
             "audio_bitrate": None,
         }
+
+    def parse_duration(self, line, duration_tag_separator):
+        try:
+            time_raw_string = line.split(duration_tag_separator)[-1]
+            match_duration = re.search(
+                r"([0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9])",
+                time_raw_string,
+            )
+            return convert_to_seconds(match_duration.group(1))
+        except Exception:
+            raise IOError(
+                (
+                    "MoviePy error: failed to read the duration of file '%s'.\n"
+                    "Here are the file infos returned by ffmpeg:\n\n%s"
+                )
+                % (self.filename, self.infos)
+            )
+
+    def parse_metadata_field_value(self, line):
+        raw_field, raw_value = line.split(":", 1)
+        return (raw_field.strip(" "), raw_value.strip(" "))
+
+    def video_metadata_type_casting(self, field, value):
+        if field == "rotate":
+            return (field, float(value))
+        return (field, value)
+
+    def update_global_data(self, global_data):
+        self.result.update(global_data)
+
+    def update_stream_data(self, stream, stream_data):
+        stream.update(stream_data)
+
+
+def convert_to_seconds(time):
+    factors = (1, 60, 3600)
+
+    if isinstance(time, str):
+        time = [float(part.replace(",", ".")) for part in time.split(":")]
+
+    if not isinstance(time, (tuple, list)):
+        return time
+
+    return sum(mult * part for mult, part in zip(factors, reversed(time)))
--- moviepy/video/io/ffmpeg_infos_parsing/parsing_state.py
+++ moviepy/video/io/ffmpeg_infos_parsing/parsing_state.py
@@ -1,3 +1,6 @@
+import re
+
+
 class ParsingState:
     def __init__(self):
         self._inside_output = False
@@ -10,3 +13,36 @@ class ParsingState:
 
     def reset(self):
         self.__init__()
+
+    def update_stream(self, stream_type, line):
+        main_info_match = re.search(
+            r"^Stream\s#(\d+):(\d+)(?:\[\w+\])?\(?(\w+)?\)?:\s(\w+):",
+            line.lstrip(),
+        )
+        input_number, stream_number, language, stream_type = main_info_match.groups()
+        input_number = int(input_number)
+        stream_number = int(stream_number)
+        stream_type_lower = stream_type.lower()
+        if language == "und":
+            language = None
+
+        self._current_stream = {
+            "input_number": input_number,
+            "stream_number": stream_number,
+            "stream_type": stream_type_lower,
+            "language": language,
+            "default": not self._default_stream_found or line.endswith("(default)"),
+        }
+        self._default_stream_found = True
+
+        return self._current_stream
+
+    def update_current_input_file(self, input_number, input_chapters):
+        if "input_number" not in self._current_input_file:
+            self._current_input_file = {"input_number": input_number, "streams": []}
+        elif self._current_input_file["input_number"] != input_number:
+            if len(input_chapters) >= input_number + 1:
+                self._current_input_file["chapters"] = input_chapters[input_number]
+            self._current_input_file = {"input_number": input_number, "streams": []}
+
+        return self._current_input_file
