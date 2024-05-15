e2b9210ee7623b15d608ef919ad37e096070bc13
Harry Hain <harryhain231@gmail.com>
2024-05-15 17:53:55 +0100

    Refactored ffmpeg_reader.py to convert instance variables to external data containers

--- moviepy/video/io/ffmpeg_reader.py
+++ moviepy/video/io/ffmpeg_reader.py
@@ -7,8 +7,9 @@ import warnings
 import numpy as np
 
 from moviepy.config import FFMPEG_BINARY  # ffmpeg, ffmpeg.exe, etc...
-from moviepy.tools import convert_to_seconds, cross_platform_popen_params
+from moviepy.tools import cross_platform_popen_params
 from moviepy.video.io.ffmpeg_reader_utils.ffmpeg_infos_parser import FFmpegInfosParser
+from moviepy.video.io.ffmpeg_reader_utils.data_container import FileInfo, VideoProperties, ProcessingState
 
 
 class FFMPEG_VideoReader:
@@ -26,8 +27,6 @@ class FFMPEG_VideoReader:
         resize_algo="bicubic",
         fps_source="fps",
     ):
-        self.filename = filename
-        self.proc = None
         infos = ffmpeg_parse_infos(
             filename,
             check_duration=check_duration,
@@ -35,52 +34,57 @@ class FFMPEG_VideoReader:
             decode_file=decode_file,
             print_infos=print_infos,
         )
-        self.fps = infos["video_fps"]
-        self.size = infos["video_size"]
 
-        # ffmpeg automatically rotates videos if rotation information is
-        # available, so exchange width and height
-        self.rotation = abs(infos.get("video_rotation", 0))
-        if self.rotation in [90, 270]:
-            self.size = [self.size[1], self.size[0]]
+        # Initialize FileInfo container
+        self.file_info = FileInfo(
+            filename=filename,
+            infos=infos,
+            duration=infos["video_duration"],
+            ffmpeg_duration=infos["duration"],
+            n_frames=infos["video_n_frames"],
+            bitrate=infos["video_bitrate"]
+        )
+
+        # Initialize VideoProperties container
+        size = infos["video_size"]
+        rotation = abs(infos.get("video_rotation", 0))
+        if rotation in [90, 270]:
+            size = [size[1], size[0]]
 
         if target_resolution:
             if None in target_resolution:
                 ratio = 1
                 for idx, target in enumerate(target_resolution):
                     if target:
-                        ratio = target / self.size[idx]
-                self.size = (int(self.size[0] * ratio), int(self.size[1] * ratio))
+                        ratio = target / size[idx]
+                size = (int(size[0] * ratio), int(size[1] * ratio))
             else:
-                self.size = target_resolution
-        self.resize_algo = resize_algo
+                size = target_resolution
 
-        self.duration = infos["video_duration"]
-        self.ffmpeg_duration = infos["duration"]
-        self.n_frames = infos["video_n_frames"]
-        self.bitrate = infos["video_bitrate"]
-
-        self.infos = infos
-
-        self.pixel_format = pixel_format
-        self.depth = 4 if pixel_format[-1] == "a" else 3
-        # 'a' represents 'alpha' which means that each pixel has 4 values instead of 3.
-        # See https://github.com/Zulko/moviepy/issues/1070#issuecomment-644457274
+        depth = 4 if pixel_format[-1] == "a" else 3
 
         if bufsize is None:
-            w, h = self.size
-            bufsize = self.depth * w * h + 100
+            w, h = size
+            bufsize = depth * w * h + 100
 
-        self.bufsize = bufsize
+        self.video_properties = VideoProperties(
+            fps=infos["video_fps"],
+            size=size,
+            rotation=rotation,
+            target_resolution=target_resolution,
+            resize_algo=resize_algo,
+            pixel_format=pixel_format,
+            depth=depth,
+            bufsize=bufsize
+        )
+
+        # Initialize ProcessingState container
+        self.processing_state = ProcessingState()
+
         self.initialize()
 
     def initialize(self, start_time=0):
-        """
-        Opens the file, creates the pipe.
-
-        Sets self.pos to the appropriate value (1 if start_time == 0 because
-        it pre-reads the first frame).
-        """
+        """Opens the file, creates the pipe."""
         self.close(delete_lastread=False)  # if any
 
         if start_time != 0:
@@ -89,12 +93,12 @@ class FFMPEG_VideoReader:
                 "-ss",
                 "%.06f" % (start_time - offset),
                 "-i",
-                self.filename,
+                self.file_info.filename,
                 "-ss",
                 "%.06f" % offset,
             ]
         else:
-            i_arg = ["-i", self.filename]
+            i_arg = ["-i", self.file_info.filename]
 
         cmd = (
             [FFMPEG_BINARY]
@@ -105,11 +109,11 @@ class FFMPEG_VideoReader:
                 "-f",
                 "image2pipe",
                 "-vf",
-                "scale=%d:%d" % tuple(self.size),
+                "scale=%d:%d" % tuple(self.video_properties.size),
                 "-sws_flags",
-                self.resize_algo,
+                self.video_properties.resize_algo,
                 "-pix_fmt",
-                self.pixel_format,
+                self.video_properties.pixel_format,
                 "-vcodec",
                 "rawvideo",
                 "-",
@@ -117,39 +121,34 @@ class FFMPEG_VideoReader:
         )
         popen_params = cross_platform_popen_params(
             {
-                "bufsize": self.bufsize,
+                "bufsize": self.video_properties.bufsize,
                 "stdout": sp.PIPE,
                 "stderr": sp.PIPE,
                 "stdin": sp.DEVNULL,
             }
         )
-        self.proc = sp.Popen(cmd, **popen_params)
+        self.processing_state.proc = sp.Popen(cmd, **popen_params)
 
         # self.pos represents the (0-indexed) index of the frame that is next in line
         # to be read by self.read_frame().
         # Eg when self.pos is 1, the 2nd frame will be read next.
-        self.pos = self.get_frame_number(start_time)
-        self.lastread = self.read_frame()
+        self.processing_state.pos = self.get_frame_number(start_time)
+        self.processing_state.lastread = self.read_frame()
 
     def skip_frames(self, n=1):
         """Reads and throws away n frames"""
-        w, h = self.size
+        w, h = self.video_properties.size
         for i in range(n):
-            self.proc.stdout.read(self.depth * w * h)
+            self.processing_state.proc.stdout.read(self.video_properties.depth * w * h)
 
-            # self.proc.stdout.flush()
-        self.pos += n
+        self.processing_state.pos += n
 
     def read_frame(self):
-        """
-        Reads the next frame from the file.
-        Note that upon (re)initialization, the first frame will already have been read
-        and stored in ``self.lastread``.
-        """
-        w, h = self.size
-        nbytes = self.depth * w * h
+        """Reads the next frame from the file."""
+        w, h = self.video_properties.size
+        nbytes = self.video_properties.depth * w * h
 
-        s = self.proc.stdout.read(nbytes)
+        s = self.processing_state.proc.stdout.read(nbytes)
 
         if len(s) != nbytes:
             warnings.warn(
@@ -159,21 +158,21 @@ class FFMPEG_VideoReader:
                     " Using the last valid frame instead."
                 )
                 % (
-                    self.filename,
+                    self.file_info.filename,
                     nbytes,
                     len(s),
-                    self.pos,
-                    self.n_frames,
-                    1.0 * self.pos / self.fps,
-                    self.duration,
+                    self.processing_state.pos,
+                    self.file_info.n_frames,
+                    1.0 * self.processing_state.pos / self.video_properties.fps,
+                    self.file_info.duration,
                 ),
                 UserWarning,
             )
-            if not hasattr(self, "last_read"):
+            if not hasattr(self.processing_state, "last_read"):
                 raise IOError(
                     (
                         "MoviePy error: failed to read the first frame of "
-                        f"video file {self.filename}. That might mean that the file is "
+                        f"video file {self.file_info.filename}. That might mean that the file is "
                         "corrupted. That may also mean that you are using "
                         "a deprecated version of FFMPEG. On Ubuntu/Debian "
                         "for instance the version in the repos is deprecated. "
@@ -181,7 +180,7 @@ class FFMPEG_VideoReader:
                     )
                 )
 
-            result = self.last_read
+            result = self.processing_state.last_read
 
         else:
             if hasattr(np, "frombuffer"):
@@ -189,62 +188,48 @@ class FFMPEG_VideoReader:
             else:
                 result = np.fromstring(s, dtype="uint8")
             result.shape = (h, w, len(s) // (w * h))  # reshape((h, w, len(s)//(w*h)))
-            self.last_read = result
+            self.processing_state.last_read = result
 
-        # We have to do this down here because `self.pos` is used in the warning above
-        self.pos += 1
+        # We have to do this down here because `self.processing_state.pos` is used in the warning above
+        self.processing_state.pos += 1
 
         return result
 
     def get_frame(self, t):
-        """Read a file video frame at time t.
-
-        Note for coders: getting an arbitrary frame in the video with
-        ffmpeg can be painfully slow if some decoding has to be done.
-        This function tries to avoid fetching arbitrary frames
-        whenever possible, by moving between adjacent frames.
-        """
-        # + 1 so that it represents the frame position that it will be
-        # after the frame is read. This makes the later comparisons easier.
+        """Read a file video frame at time t."""
         pos = self.get_frame_number(t) + 1
 
         # Initialize proc if it is not open
-        if not self.proc:
+        if not self.processing_state.proc:
             print("Proc not detected")
             self.initialize(t)
-            return self.last_read
+            return self.processing_state.last_read
 
-        if pos == self.pos:
-            return self.last_read
-        elif (pos < self.pos) or (pos > self.pos + 100):
-            # We can't just skip forward to `pos` or it would take too long
+        if pos == self.processing_state.pos:
+            return self.processing_state.last_read
+        elif (pos < self.processing_state.pos) or (pos > self.processing_state.pos + 100):
             self.initialize(t)
-            return self.lastread
+            return self.processing_state.lastread
         else:
-            # If pos == self.pos + 1, this line has no effect
-            self.skip_frames(pos - self.pos - 1)
+            self.skip_frames(pos - self.processing_state.pos - 1)
             result = self.read_frame()
             return result
 
     def get_frame_number(self, t):
         """Helper method to return the frame number at time ``t``"""
-        # I used this horrible '+0.00001' hack because sometimes due to numerical
-        # imprecisions a 3.0 can become a 2.99999999... which makes the int()
-        # go to the previous integer. This makes the fetching more robust when you
-        # are getting the nth frame by writing get_frame(n/fps).
-        return int(self.fps * t + 0.00001)
+        return int(self.video_properties.fps * t + 0.00001)
 
     def close(self, delete_lastread=True):
         """Closes the reader terminating the process, if is still open."""
-        if self.proc:
-            if self.proc.poll() is None:
-                self.proc.terminate()
-                self.proc.stdout.close()
-                self.proc.stderr.close()
-                self.proc.wait()
-            self.proc = None
-        if delete_lastread and hasattr(self, "last_read"):
-            del self.last_read
+        if self.processing_state.proc:
+            if self.processing_state.proc.poll() is None:
+                self.processing_state.proc.terminate()
+                self.processing_state.proc.stdout.close()
+                self.processing_state.proc.stderr.close()
+                self.processing_state.proc.wait()
+            self.processing_state.proc = None
+        if delete_lastread and hasattr(self.processing_state, "last_read"):
+            del self.processing_state.last_read
 
     def __del__(self):
         self.close()
